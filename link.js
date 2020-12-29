9bcdf8336b3922dfae2f4f33dd7429b6b2df59fa
Avaer Kazmer <a@modules.io>
2020-12-29 10:34:37 -0500

    More debugging

--- link.js
+++ link.js
@@ -1,13 +1,22 @@
 import storage from './storage.js';
+import bip39 from './bip39.js';
+import hdkeySpec from './hdkey.js';
+const hdkey = hdkeySpec.default;
 
 (async () => {
   const loginToken = await storage.get('loginToken');
+  console.log('got', loginToken);
   const mnemonic = loginToken ? loginToken.mnemonic : null;
 
+  const headerMessageEl = document.getElementById('header-message');
   const device1El = document.getElementById('device-1');
   const device2El = document.getElementById('device-2');
 
   if (mnemonic) {
+    const wallet = hdkey.fromMasterSeed(bip39.mnemonicToSeedSync(mnemonic)).derivePath(`m/44'/60'/0'/0/0`).getWallet();
+    const address = wallet.getAddressString();
+    headerMessageEl.innerText = `Logged in as: ${address}`;
+
     device1El.classList.remove('disabled');
     device1El.addEventListener('click', async () => {
       const res = await fetch(`https://login.exokit.org?autoip=src&mnemonic=${mnemonic}`, {
@@ -32,7 +41,7 @@ import storage from './storage.js';
       const j = await res.json();
       console.log('got auto login dst', j);
       const {mnemonic} = j;
-      await storage.set('loginToken', JSON.stringify({mnemonic}));
+      await storage.set('loginToken', {mnemonic});
     } else {
       console.warn(res.status);
     }
