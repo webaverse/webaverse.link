5f91645505583f744ecac7770f39d324ef17b8f1
Avaer Kazmer <a@modules.io>
2020-12-29 08:55:22 -0500

    Code

--- link.js
+++ link.js
@@ -1,1 +1,17 @@
-console.log('link');
+document.getElementById('device-1').addEventListener('click', async () => {
+  const res = await fetch(`https://login.exokit.org?autoip=src`, {
+    method: 'POST',
+    body: JSON.stringify({
+      mnemonic: 'lol',
+    }),
+  });
+  const j = await res.json();
+  console.log('got auto login src', j);
+});
+document.getElementById('device-2').addEventListener('click', async () => {
+  const res = await fetch(`https://login.exokit.org?autoip=dst`, {
+    method: 'POST',
+  });
+  const j = await res.json();
+  console.log('got auto login dst', j);
+});
